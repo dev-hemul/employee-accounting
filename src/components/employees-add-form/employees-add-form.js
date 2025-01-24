44fd878bf0754eedbede5aa7e546fc9353f4d078
dev-hemul <dev.hemulll@gmail.com>
2025-01-24 10:40:21 +0200

    done project

---
+++ src/components/employees-add-form/employees-add-form.js
@@ -0,0 +1,68 @@
+import {Component} from 'react';
+
+import './employees-add-form.css';
+
+class EmployeesAddForm extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: '',
+			salary: ''
+		}
+	}
+	
+	onValueChange = (e) => {
+		this.setState({
+			[e.target.name]: e.target.value
+		})
+	}
+	
+	onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onAdd(this.state.name, this.state.salary);
+        this.setState({
+            name: '',
+            salary: ''
+        })
+    }
+	
+	
+	
+	render() {
+		const {name, salary} = this.state;
+		return (
+		<div className="app-add-form">
+			<h3>Додати нового співробітника</h3>
+			<form
+				className="add-form d-flex"
+				onSubmit = {this.onSubmit}
+			>
+				<input type="text"
+				       className="form-control new-post-label"
+				       placeholder="Як його звуть?"
+				       name="name"
+				       value={name}
+				       onChange={this.onValueChange}
+				       required
+				/>
+				<input type="number"
+				       className="form-control new-post-label"
+				       placeholder="З/П в $?"
+				       name="salary"
+				       value={salary}
+				       onChange={this.onValueChange}
+				       required
+				/>
+				
+				<button type="submit"
+				        className="btn btn-outline-light"
+				>
+					Добавити
+				</button>
+			</form>
+		</div>
+	)
+	}
+}
+
+export default EmployeesAddForm;
